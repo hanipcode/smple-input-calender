ea080b989760fbac977c43b98ee424927f222031
hanipcode <hanifeij@yahoo.co.id>
2023-06-02 12:05:19 +0700

    add example

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { PasswordInput } from "./components/PasswordInput";
 
 function App() {
   return (
-    <div className="bg-dark h-screen w-full p-5 ">
-      {/* <PasswordInput label="Password" /> */}
-      <Datepicker placeholder="Birthday" />
+    <div className="bg-dark h-screen w-full p-5 flex">
+      <div className="flex-1">
+        <PasswordInput label="Password" />
+      </div>
+      <div className="flex-1">
+        <Datepicker placeholder="Birthday" />
+      </div>
     </div>
   );
 }
